801ba95394747968f3719305108080c23636394a
Aketzalli1 <125938119+Aketzalli1@users.noreply.github.com>
2023-05-01 13:28:26 -0600

    Add files via upload

---
+++ Tarea_12/Main.java
@@ -0,0 +1,10 @@
+package Tarea_12;
+
+import Tarea_12.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+
+    }
+}
