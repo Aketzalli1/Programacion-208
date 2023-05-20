bcee6f9db47fdfe64313b4dac7d1f6168b831ff7
Aketzalli1 <125938119+Aketzalli1@users.noreply.github.com>
2023-05-20 15:28:31 -0600

    Add files via upload

---
+++ Ext_9/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Controlador.ControladorLibro;
+import org.example.Vista.VentanaLibro;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaLibro view = new VentanaLibro("MVC Y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
+    }
+}
